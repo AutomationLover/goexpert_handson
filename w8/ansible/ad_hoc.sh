667e8ed642ae883c57cd63955c7d6d950ad1830e
William Wang <hahawangxiao@gmail.com>
2024-03-14 18:31:31 +1100

    Update ad_hoc.sh

--- w8/ansible/ad_hoc.sh
+++ w8/ansible/ad_hoc.sh
@@ -1,19 +1,71 @@
-# Rebooting servers
-ansible all -a "sudo /sbin/reboot"
+Ansible ad-hoc commands are a powerful yet simple way to perform quick tasks, especially during the development phase. Here are few examples:
 
-# Managing files
-ansible tag_node -m copy -a "src=/etc/hosts dest=/tmp/hosts"
+1. **Ping all hosts:** 
 
-# Managing packages
-ansible tag_node -m apt -a "name=zip state=present" --become --become-method=sudo
-ansible tag_node -m apt -a "name=zip state=absent" --become --become-method=sudo
-ansible tag_node -m apt -a "name=nginx state=present" --become --become-method=sudo
+    ```bash
+    ansible all -m ping
+    ```
+    This command will run a ping task on all hosts in your inventory.
 
-# Managing users
-ansible tag_node -m user -a "name=foo" --become --become-method=sudo
-ansible tag_node -m user -a "name=foo state=absent" --become --become-method=sudo
+2. **Gather facts from all hosts:**
 
-# Managing services
-ansible tag_node -m service -a "name=nginx state=stopped" --become --become-method=sudo
-ansible tag_node -m service -a "name=nginx state=started" --become --become-method=sudo
-ansible tag_node -m service -a "name=nginx state=restarted" --become --become-method=sudo
+    ```bash
+    ansible all -m setup
+    ```
+    This command will gather facts (system and setup information) about all of the servers you are managing.
+
+3. **Manage packages using apt:**
+
+    ```bash
+    ansible all -b -m apt -a "name=nginx state=present"
+    ```
+    This command will make sure the nginx package is installed on all servers.
+
+4. **Manage services with systemd module:**
+
+    ```bash
+    ansible all -m systemd -a "name=nginx state=started"
+    ```
+    Here `-b` or `--become` tells ansible to run operations with become (privilege escalation). or you can use " --become --become-method=sudo"
+    This command will make sure the nginx service is started on all servers.
+
+5. **Create a user on all servers:**
+
+    ```bash
+    ansible all -b -m user -a "name=testuser password={{ 'mysecretpassword' | password_hash('sha512') }}"
+    ```
+    This command will create a user 'testuser' with the specified password on all servers.
+
+6. **Copy a file to all servers:**
+
+    ```bash
+    ansible all -m copy -a "src=/etc/hosts dest=/tmp/hosts"
+    ```
+    This command will copy the /etc/hosts file from your system to /tmp/hosts location on all the servers.
+
+7. **Delete a file from all servers:**
+
+    ```bash
+    ansible all -m file -a "path=/tmp/sample.txt state=absent"
+    ```
+    This command will delete /tmp/sample.txt from all servers.
+
+8. **Restart a service on all servers:**
+
+    ```bash
+    ansible all -m apt -a "name=nginx state=present"
+    ```
+    This command will restart the nginx service on all servers.
+
+9. **Execute a command on all servers:**
+
+    ```bash
+    ansible all -m command -a "uptime"
+    ```
+    This command will run 'uptime' on all servers and return the result. 
+
+Please replace `all` with your desired group or host in all above commands. Remember, you need to have a proper inventory file set up for these commands to work.
+
+
+Official Docs for Ansible module
+https://docs.ansible.com/ansible/latest/collections/index_module.html
