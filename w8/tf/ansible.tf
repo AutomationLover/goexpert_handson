4846693d070cd4bfabd3c255bc96d1eb55c87e0d
William Wang <hahawangxiao@gmail.com>
2025-02-20 14:10:24 +1100

    Update ansible.tf

--- w8/tf/ansible.tf
+++ w8/tf/ansible.tf
@@ -32,7 +32,22 @@ data "aws_ami" "amazon_linux" {
 
   filter {
     name   = "name"
-    values = ["al2023-ami-2023.6.20241010.0-kernel-6.1-x86_64"]
+    values = ["al2023-ami-*-x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
   }
 }
 
