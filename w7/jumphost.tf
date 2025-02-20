bf6e4e21985664e54f3ef07878bb68b97e2027b2
William Wang <hahawangxiao@gmail.com>
2025-02-20 14:11:20 +1100

    Update jumphost.tf

--- w7/jumphost.tf
+++ w7/jumphost.tf
@@ -35,7 +35,22 @@ data "aws_ami" "amazon_linux" {
 
   filter {
     name   = "name"
-    values = ["al2023-ami-2023.5.20240708.0-kernel-6.1-x86_64"]
+    values = ["al202*-ami-*-x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
   }
 }
 
